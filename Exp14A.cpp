8281e6d69305aa86e9dcebe849c8f66b9fcc47e6
Pranjal Panwar <138666385+pranjalpanwar81@users.noreply.github.com>
2024-09-11 09:53:43 +0530

    Create Exp14A.cpp

---
+++ Exp14A.cpp
@@ -0,0 +1,33 @@
+// NAME - Pranjal Panwar 
+// PRN - 23070123164 
+// EXPERIMENT - 14(A)  
+
+# include <iostream>
+# include <string>
+using namespace std;
+
+class Uni
+{
+    public:
+    string uni= "Symbiosis";
+    void discipline()
+    {
+        cout << "Engineering" <<endl;
+    }
+};
+class Dep : public Uni
+{
+    public:
+    string dept= " Electronics & Telecommunication";
+};
+int main()
+{
+    Dep u1;
+    u1.discipline();
+    cout<<u1.uni + " "+u1.dept ;
+} 
+
+// Output 
+// 
+// Engineering
+// Symbiosis  Electronics & Telecommunication
